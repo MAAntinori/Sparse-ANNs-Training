995c92a8c2926e9b757a0148aa7689a6bf701a16
MAAntinori <80471656+MAAntinori@users.noreply.github.com>
2023-11-24 10:24:36 +0100

    Add files via upload

---
+++ visualizing.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = pd.read_excel('model_results_8x8_epochs.xlsx', decimal=',')
+
+# Convert 'Accuracy' column to numeric (remove commas and convert to float)
+#df['Accuracy'] = pd.to_numeric(df['Accuracy'].str.replace(',', '.'))
+
+# Loop through unique combinations of 'Model' and 'Quantized'
+for model, quantized in df.groupby(['Model', 'Quantized']):
+    model_id = model[0]
+    quantized_flag = 'Quantized' if model[1].any() else 'Not Quantized'
+
+    # Create a pivot table
+    heatmap_data = quantized.pivot_table(values='Accuracy', index='Epochs', columns='Sparsity')
+     # Sort the index in descending order
+    heatmap_data = heatmap_data.sort_index(ascending=False)
+
+    # Create a heatmap using Seaborn
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(heatmap_data, annot=True, cmap='viridis', fmt='.3f', linewidths=.5)
+    plt.title(f'Model {model_id} - {quantized_flag} - Accuracy Heatmap')
+    plt.show()
